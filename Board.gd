fc78a57a8d356f6d3779bd2bfae46ed776a8c810
Richard Eigenmann <richard.eigenmann@gmail.com>
2018-02-18 18:49:36 +0100

    Adding most game logic as per Wikipedia

--- Board.gd
+++ Board.gd
@@ -48,13 +48,16 @@ func _on_ComputerThinkTimer_timeout():
 	computerTurn()
 
 
+# Logic taken from Wikipedia: https://en.wikipedia.org/wiki/Tic-tac-toe
 func computerTurn():
 	rounds += 1
 	if ! computerCanWin():
-		for tile in tiles:
-			if tile.state == EMPTY:
-				tile.setState(computerState)
-				break
+		if ! computerMustBlock():
+			if ! computerCreatesFork():
+				if ! compterTakesCenter():
+					if ! computerTakesOppositeCorner():
+						if ! computerTakesEmptyCorner():
+							computerTakesEmptySide()
 
 	if isWinner(computerState):
 		$WinnerLabel.text = "Computer\nwins!"
@@ -109,7 +112,7 @@ func computerCanWinCol( col ):
 	elif (tiles[col].state == computerState) && (tiles[col+3].state == EMPTY) && (tiles[col+6].state == computerState):
 		tiles[col+3].setState(computerState)
 		return true
-	elif (tiles[col].state == EMPTY) && (tiles[col+3].state == computerState) && (tiles[col+8].state == computerState):
+	elif (tiles[col].state == EMPTY) && (tiles[col+3].state == computerState) && (tiles[col+6].state == computerState):
 		tiles[col].setState(computerState)
 		return true
 
@@ -132,7 +135,110 @@ func computerCanWinDiagonals():
 	elif (tiles[2].state == EMPTY) && (tiles[4].state == computerState) && (tiles[6].state == computerState):
 		tiles[2].setState(computerState)
 		return true
-		
+
+func computerMustBlock():
+	for row in range(3):
+		if computerMustBlockRow( row ):
+			return true
+	for col in range(3):
+		if computerMustBlockCol( col ):
+			return true
+	return computerMustBlockDiagonals()
+
+func computerMustBlockRow( row ):
+	if (tiles[row*3].state == playerState) && (tiles[row*3+1].state == playerState) && (tiles[row*3+2].state == EMPTY):
+		tiles[row*3+2].setState(computerState)
+		return true
+	elif (tiles[row*3].state == playerState) && (tiles[row*3+1].state == EMPTY) && (tiles[row*3+2].state == playerState):
+		tiles[row*3+1].setState(computerState)
+		return true
+	elif (tiles[row*3].state == EMPTY) && (tiles[row*3+1].state == playerState) && (tiles[row*3+2].state == playerState):
+		tiles[row*3].setState(computerState)
+		return true
+	return false
+
+func computerMustBlockCol( col ):
+	if (tiles[col].state == playerState) && (tiles[col+3].state == playerState) && (tiles[col+6].state == EMPTY):
+		tiles[col+6].setState(computerState)
+		return true
+	elif (tiles[col].state == playerState) && (tiles[col+3].state == EMPTY) && (tiles[col+6].state == playerState):
+		tiles[col+3].setState(computerState)
+		return true
+	elif (tiles[col].state == EMPTY) && (tiles[col+3].state == playerState) && (tiles[col+8].state == playerState):
+		tiles[col].setState(computerState)
+		return true
+
+func computerMustBlockDiagonals():
+	if (tiles[0].state == playerState) && (tiles[4].state == playerState) && (tiles[8].state == EMPTY):
+		tiles[8].setState(computerState)
+		return true
+	elif (tiles[0].state == playerState) && (tiles[4].state == EMPTY) && (tiles[8].state == playerState):
+		tiles[4].setState(computerState)
+		return true
+	elif (tiles[0].state == EMPTY) && (tiles[4].state == playerState) && (tiles[8].state == playerState):
+		tiles[0].setState(computerState)
+		return true
+	if (tiles[2].state == playerState) && (tiles[4].state == playerState) && (tiles[6].state == EMPTY):
+		tiles[6].setState(computerState)
+		return true
+	elif (tiles[2].state == playerState) && (tiles[4].state == EMPTY) && (tiles[6].state == playerState):
+		tiles[4].setState(computerState)
+		return true
+	elif (tiles[2].state == EMPTY) && (tiles[4].state == playerState) && (tiles[6].state == computerState):
+		tiles[2].setState(computerState)
+		return true
+
+func computerCreatesFork():
+	print ("Function computerCreatesFork needs implementation!");
+	return false
+
+func compterTakesCenter():
+	if tiles[4].state == EMPTY:
+		tiles[4].setState(computerState)
+		return true
+	return false
+
+func computerTakesOppositeCorner():
+	if (tiles[0].state == playerState) && (tiles[8].state == EMPTY):
+		tiles[8].setState(computerState)
+		return true
+	if (tiles[2].state == playerState) && (tiles[6].state == EMPTY):
+		tiles[6].setState(computerState)
+		return true
+	if (tiles[8].state == playerState) && (tiles[0].state == EMPTY):
+		tiles[0].setState(computerState)
+		return true
+	if (tiles[6].state == playerState) && (tiles[2].state == EMPTY):
+		tiles[2].setState(computerState)
+		return true
+	
+func computerTakesEmptyCorner():
+	if tiles[0].state == EMPTY:
+		tiles[0].setState(computerState)
+		return true
+	if tiles[2].state == EMPTY:
+		tiles[2].setState(computerState)
+		return true
+	if tiles[8].state == EMPTY:
+		tiles[8].setState(computerState)
+		return true
+	if tiles[6].state == EMPTY:
+		tiles[6].setState(computerState)
+		return true
+
+func computerTakesEmptySide():
+	if tiles[1].state == EMPTY:
+		tiles[1].setState(computerState)
+		return true
+	if tiles[3].state == EMPTY:
+		tiles[3].setState(computerState)
+		return true
+	if tiles[5].state == EMPTY:
+		tiles[5].setState(computerState)
+		return true
+	if tiles[7].state == EMPTY:
+		tiles[7].setState(computerState)
+		return true
 
 func printStates():
 	print ("PrintStates")
